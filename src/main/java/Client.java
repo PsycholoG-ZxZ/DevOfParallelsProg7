fcfe8a9a5ac0b59873e22eb65d2267714bd0ddf6
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 21:54:50 +0300

    Scripted auto-commit on change (2019-12-27 21:54:50) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,18 +2,30 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args){
         ZContext context = new ZContext();
         ZMQ.Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+
         try{
             System.out.println("connect");
             socket = context.createSocket(SocketType.REQ);
             socket.connect("tcp://localhost:5555");
-            for (int i = 0; i < 10; i++){
-                socket.send("request" + i, 0);
-                String reply = socket.recvStr();
-                System.out.println("reply" + i +" result = "+ reply);
+            //while(!Thread.currentThread().isInterrupted()){ // or
+
+            while (true){
+                String mes = scanner.nextLine();
+                if (mes.equals("EXIT")){
+                    break;
+                }
+                if (mes.contains("GET") && mes.contains("PUT")){
+                    socket.send("request" + i, 0);
+                }
+
+
             }
         }finally {
             context.destroySocket(socket);
