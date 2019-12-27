aa886d6a69bb269dac15f08b43a928aefdfaa694
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 22:36:32 +0300

    Scripted auto-commit on change (2019-12-27 22:36:32) by gitwatch.sh

--- src/main/java/CacheStorage.java
+++ src/main/java/CacheStorage.java
@@ -1,13 +1,17 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CacheStorage {
     public static void main(String[] args){
         //key-value
-        Map<Integer,String> cache;
+        Map<Integer,String> cache = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         for (int i = scanner.nextInt(); i <= scanner.nextInt(); i++){
-            cache.put(i, )
+            cache.put(i, "");
+        }
+        try {
+
         }
     }
 }
