098fb2b34dd716cf6d2ce9e3b90234320dac66c3
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-28 06:27:27 +0300

    Scripted auto-commit on change (2019-12-28 06:27:27) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -25,6 +25,7 @@ public class ProxyServer {
                 }
                 if (poller.pollin(1)){
                     ZMsg msg = ZMsg.recvMsg(backend);
+                    
                 }
             }
         }
