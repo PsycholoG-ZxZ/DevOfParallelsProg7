72ae4b5434fb00011ba9191ad0c788c413783141
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-28 05:40:22 +0300

    Scripted auto-commit on change (2019-12-28 05:40:22) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -14,7 +14,8 @@ public class ProxyServer {
             frontend.bind("tcp://localhost:5555");
             backend.bind("tcp://localhost:6665");
             ZMQ.Poller poller =context.createPoller(2);
-            
+
+            poller.register()
         }
 
     }
