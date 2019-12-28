138e21ebeb852e3869bc6f2690869fbd7a085bea
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-28 12:12:29 +0300

    Scripted auto-commit on change (2019-12-28 12:12:29) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -28,7 +28,7 @@ public class ProxyServer {
             while (!Thread.currentThread().isInterrupted()){
                 poller.poll();
                 if (!frameAndCacheMap.isEmpty() && System.currentTimeMillis() - time > 5000){
-                    for (Iterator<Map.Entry>
+                    for (Iterator<Map.Entry<ZFrame, DataCache>>
                 }
                 if (poller.pollin(0)){
                     ZMsg msg = ZMsg.recvMsg(frontend);
