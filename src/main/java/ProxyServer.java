f9b82b736808cae2a367742d21e0835cc4498091
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-28 05:30:03 +0300

    Scripted auto-commit on change (2019-12-28 05:30:03) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -5,9 +5,9 @@ import java.net.Socket;
 public class ProxyServer {
 
     public static void main(String[] args){
-
+        ZContext context = new ZContext();
         try {
-            ZContext context = new ZContext();
+
             Socket frontend = context.createSocket(SocketType.ROUTER);
             Socket backend = context.createSocket(SocketType.ROUTER);
         }
