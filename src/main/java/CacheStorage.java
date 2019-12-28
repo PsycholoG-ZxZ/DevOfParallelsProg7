790904f9493be067513261ad76a61274620de3f9
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-28 14:57:15 +0300

    Scripted auto-commit on change (2019-12-28 14:57:14) by gitwatch.sh

--- src/main/java/CacheStorage.java
+++ src/main/java/CacheStorage.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public static 
+public static final 
 
 public class CacheStorage {
     public static void main(String[] args){
