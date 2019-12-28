650b21c2e62b5c56902f23f88f180994692b5bfc
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-28 12:46:14 +0300

    Scripted auto-commit on change (2019-12-28 12:46:14) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -31,7 +31,7 @@ public class ProxyServer {
                 if (!frameAndCacheMap.isEmpty() && System.currentTimeMillis() - time > 5000){
                     for (Iterator<Map.Entry<ZFrame, DataCache>> iter = frameAndCacheMap.entrySet().iterator(); iter.hasNext();){
                         Map.Entry<ZFrame, DataCache> entry = iter.next();
-                        if (Math.abs(entry.getValue().getTime() - time) > 5000 *2){
+                        if (Math.abs(entry.getValue().getTime() - time) > 5000 * 5){
                             System.out.println("Cache deleted" + entry.getKey());
                             iter.remove();
                         }
